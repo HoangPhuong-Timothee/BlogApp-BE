85597c4fc97a1c3dbcdc365fd3589458d78fe7a2
19522059 <19522059@gm.uit.edu.vn>
2023-04-26 16:13:31 +0700

    Add upload image to local storage?

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const userRoute = require('./routes/user_route');
 const postRoute = require('./routes/post_route');
 const categoryRoute = require('./routes/category_route');
 const authRoute = require('./routes/auth_route');
+const multer = require('multer');
 
 //DB connection
 dbConnection();
@@ -18,9 +19,17 @@ app.use(bodyParser.json({ limit: "30mb" }));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
 
+const storage = multer.diskStorage({ destination: (req, file, cb)=>{
+    cb(null, "imgage");
+    },
+    filename: (req, file, cb)=>{
+        cb(null, req.body.name);
+    }
+});
+const upload = multer({ storage: storage });
 //routes
-app.get('/', (req, res)=>{
-    res.send("Server-side");
+app.use('/api/upload', upload.single("file"), (req, res)=>{
+    res.status(200).json("File has been uploaded!!!");
 });
 app.use('/api/auth', authRoute);
 app.use('/api/user', userRoute);
